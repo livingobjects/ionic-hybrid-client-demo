d4a3c18445c5403c40c7d750e37b7549b600ba00
shprink <julien.renaux@gmail.com>
2015-02-13 00:06:37 +0100

    adding link to product

--- www/js/app.js
+++ www/js/app.js
@@ -71,6 +71,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             "b.lo": {
                 name: 'b.lo',
                 img: 'http://livingobjects.com/api/images/253/x100',
+                link: 'http://livingobjects.com/products/b-lo.html',
                 title: 'Analytics Software Suite for Network Management',
                 excerpt: '<p>b.lo™ is a modular analytics software platform loaded with extensive Network Operation expertise. It uniquely combines <b>Network Performance Management</b> excellence, with business-oriented capabilities to bring unrivalled value to operators and their end-customers.</p>',
                 description: '<p>All kinds of analysis can be done simultaneously by all categories of users, using the palette of advanced network tools or interpretive visualization features. In a few clicks, b.lo™ provides comprehensive information that helps both engineering teams troubleshoot complex problems and customer care communicate with simple words.</p><p>Browse the different domains. Explore real-time and historical data side-by-side. Drill-down into the details of your Network. Mix the 4G and VOD traffic on a single graph. Visualize the TOP 10 applications on an IP Network. Aggregate Network Element from a geographical cluster in a single graph. Selected views can then be saved as a template for later health-checks or for reports.</p>'
@@ -78,6 +79,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             "eye.lo": {
                 name: 'eye.lo',
                 img: 'http://livingobjects.com/api/images/286/x100',
+                link: 'http://livingobjects.com/products/eye-lo.html',
                 title: 'Application Visibility and Performance Platform',
                 excerpt: '<p>eye.lo™ is a multi-tenant platform that deliver <b>Service Providers</b> with a powerful application-aware management platform, helping them to assure the delivery of WAN connectivity services to their business customers.</p>',
                 description: '<p>LivingObjects is pioneering a new way of managing Wide Area Networks (WANs) and the broadband access services provided to enterprise customers. eye.lo™ is a software-only (probe-less) solution takes full advantage of the latest features embedded in routers and leverages the expertise of LivingObjects in Network Performance Management, big-data processing, analytics and interactive user-interface.</p><p>From the thousands of recognized applications or visited web sites, eye.lo™ reports, with an interactive dashboard, the top applications and urls making the bulk of the traffic: Business/leisure applications, web-sites categories, criteria for Top Users, Top conversations, CoS policies between sites, and much more.</p>'
@@ -85,6 +87,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
             "app.lo": {
                 name: 'app.lo',
                 img: 'http://livingobjects.com/api/images/287/x100',
+                link: 'http://livingobjects.com/products/app-lo.html',
                 title: 'Monitor the pulse of your Networks with app.lo',
                 excerpt: '<p>app.lo is a decision-support tablet application targeted at Telecom Executives and decision makers. This <b>Business Intelligence</b> embedded application creates an abstract from the global data set of a Network Management System (such as b.lo or others). It enables the user on the move to remain informed with essential network data and communicate with the teams in the field to make quick and efficient decisions.</p>',
                 description: '<p>app.lo lets you configure data access according to your organization : Devices, profile, data. You specify via a web-service, the data you want to keep track of in the tablet, coming from .lo solutions or other applications. Through a secure, link data will be automatically download, as soon as the device is connected. The information is available even if the connection is off.</p><p>Choose your KPI, zoom on selected areas, scroll through the history, identify TOPs. Visualize information on a map and assess the outage impact. Get detailed characteristics of pointed site locations. Spot the issue you want to focus on and send it by e-mail. app.lo provides near real-time analysis capability and signals important changes in business conditions (Networks availability, Incidents,..).</p>'
